2a17006f943339f44ea677ffe61f0cd73bfdb785
Michael Holod <git_ara@ukr.net>
2021-11-28 01:10:47 +0200

    File 'webpack.mix.js' has been changed

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,8 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+/*mix.js('resources/js/app.js', 'public/js')
+	.postCss('resources/css/app.css', 'public/css', [
         //Include tailwind css
 		require('tailwindcss')
-    ]);
+    ]);*/
+    
+mix.js('resources/js/app.js', 'public/js')
+	.sass('resources/css/site/app.scss', 'public/css')
+	//.styles(['resources/css/site/app.css'], 'public/css/app.css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')]
+	});
