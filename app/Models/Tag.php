80451460c15e9a91509a1b7d3c52ff1a57d0695d
Michael Holod <git_ara@ukr.net>
2021-12-09 01:28:10 +0200

    Model, Factory and Migration have been created for tag. Tag model has a relationship many-to-many to 'articles'

---
+++ app/Models/Tag.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+	
+	//
+	protected $fillable = ['label'];
+	
+	public $timestamps = false;
+	
+	//Many to Many using the pivot table 'article_tag'
+	public function articles(){
+		return $this->belongsToMany(Article::class);
+	}
+}
