cbc56c97009747a2df14f86362e6a1bc6fc21532
Michael Holod <git_ara@ukr.net>
2021-12-21 04:09:49 +0200

    The 'app.js' file has been modified in 'resource/js' folder to import and register Vue root components. Also in 'resource/js' the folder 'components' was created to store Vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+import Vue from 'vue';
+
+//Load components by root sections: Header, Sidebar, Content, Footer
+import Content from './components/Content.vue';
+
+const app = new Vue({
+    el: '#app',
+    components: { MxContent: Content }
+});
---
+++ resources/js/components/Content.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="my-vue">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {},
+  data() {
+      return {
+          message:'Content !!!'
+      }
+  }
+};
+</script>
