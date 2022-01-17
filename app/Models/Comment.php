cc71a8217fb2f990bc9d766f8f953e6057597783
Michael Holod <git_ara@ukr.net>
2022-01-17 19:19:06 +0200

    The model 'Comment' has set up relationship with the 'User' model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,7 +10,7 @@ class Comment extends Model
     use HasFactory;
 	
 	//Accessible fields during mass fillings
-	protected $fillable = ['subject', 'body', 'article_id'];
+	protected $fillable = ['subject', 'body', 'article_id', 'user_id'];
 	
 	//Inaccessible fields during mass fillings
 	//protected $guarded = [];
@@ -18,5 +18,13 @@ class Comment extends Model
 	//One To Many (Inverse). Comment belongs to an Article
 	public function article(){
 		return $this->belongsTo(Article::class);
+	}
+	public function createdAtForHumans(){
+		//Using PHP Carbon library to work with Date and Time. It is built in Laravel.
+		return $this->created_at->diffForHumans();
+	}
+	//One to One
+	public function user(){
+		return $this->hasOne(User::class,'id','user_id');
 	}
 }
