acff21fe28487528ead16143938592938be4cafc
Michael Holod <git_ara@ukr.net>
2022-01-17 18:35:39 +0200

    The article model has been changed. Added several 'scope' methods to work with 'statistics', 'tags', 'comments'

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
@@ -17,7 +18,7 @@ class Article extends Model
 	
 	//One to Many. One article has many comments
 	public function comments(){
-		return $this->hasMany(Comment::class);
+		return $this->hasMany(Comment::class,'article_id', 'id');
 	}
 	//One to One
 	public function statistics(){
@@ -26,5 +27,32 @@ class Article extends Model
 	//Many to Many using the pivot table 'article_tag'
 	public function tags(){
 		return $this->belongsToMany(Tag::class);
+	}
+	public function getBodyPreview(){
+		//Laravel helper 'limit()' function
+		return Str::limit($this->body, 100);
+	}
+	
+	public function createdAtForHumans(){
+		//Using PHP Carbon library to work with Date and Time. It is built in Laravel.
+		return $this->created_at->diffForHumans();
+	}
+	
+	public function scopeLastLimit($query, $amount){
+		//Eager loading 'with' method. Use 'take' or 'limit'
+		return $query->with('statistics','tags')->orderBy('created_at','desc')->take($amount)->get();
+	}
+	
+	public function scopeAllPaginate($query, $amount){
+		return $query->with('statistics','tags')->orderBy('created_at', 'desc')->paginate($amount);
+	}
+	
+	public function scopeFindBySlug($query, $slug){
+		//Method 'firstOrFail' finds first article all invokes an error 
+		return $query->with('statistics','tags','comments')->where('slug', $slug)->firstOrFail();
+	}
+	
+	public function scopeFindByTag($query, $amount){
+		return $query->with('statistics','tags')->orderBy('created_at', 'desc')->paginate($amount);
 	}
 }
