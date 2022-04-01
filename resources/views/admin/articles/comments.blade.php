397c6a85a5e273009e1de5826b5c5bf19c8d2b85
Michael Holod <git_ara@ukr.net>
2022-04-01 23:37:23 +0300

    A template has been created that displays a list of related comments to the article

---
+++ resources/views/admin/articles/comments.blade.php
@@ -0,0 +1,32 @@
+@extends('admin.layouts.index')
+
+@section('content')
+<div class="tw-container tw-bg-white tw-p-6 tw-rounded-l-lg">
+	<h3 class="tw-mt-2 tw-mb-2 tw-text-blue-600 tw-font-black">
+		List of comments to the article
+	</h3>
+	<ul class="list-group">
+		@foreach($comments as $comment)
+			@if ($loop->first)
+				<li class="list-group-item disabled" aria-disabled="true">
+					<div><b>User: </b>{{ $comment->user->firstname }}</div>
+					<div>
+						<b>Subject: </b>
+						{{ $comment->subject }}
+					</div>
+					<div><b>Comment: </b>{{ $comment->body }}</div>
+				</li>
+			@else
+			<li class="list-group-item">
+				<div><b>User: </b>{{ $comment->user->firstname }}</div>
+				<div>
+					<b>Subject: </b>
+					{{ $comment->subject }}
+				</div>
+				<div><b>Comment: </b>{{ $comment->body }}</div>
+			</li>
+			@endif
+		@endforeach
+	</ul>
+</div>
+@endsection
