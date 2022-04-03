c8d0067515fcd985f560e46811e5d1c7c686c9ef
Michael Holod <git_ara@ukr.net>
2022-04-04 02:03:16 +0300

    Main page of administration has been prepared

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -1,7 +1,10 @@
 @extends('admin.layouts.index')
 
+@section('title','Main page of administration')
+
 @section('content')
 <div class="tw-container tw-bg-white tw-p-6 tw-rounded-l-lg">
 	<p>Admin Home</p>
+	
 </div>
 @endsection
