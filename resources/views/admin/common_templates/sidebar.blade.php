c997a8c691073213a83c152c38de5703b93aabe3
Michael Holod <git_ara@ukr.net>
2022-04-08 18:54:03 +0300

    The sidebar section contains link to the article. It allows to create, edit and delete the article

--- resources/views/admin/common_templates/sidebar.blade.php
+++ resources/views/admin/common_templates/sidebar.blade.php
@@ -1,5 +1,14 @@
 <aside class="tw-bg-white tw-p-2 tw-rounded-r-lg">
-	<h3>Sidebar</h3>
+	<div class="tw-border-2 tw-border-indigo-300 tw-border-solid tw-rounded tw-p-1">
+		<h3 class="tw-text-center tw-font-black">Entity management </h3>
+		<ul class="nav flex-column">
+			<li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="{{ route('articles.index') }}">
+					Articles
+				</a>
+			</li>
+		</ul>
+	</div>
 </aside>
 @push('scripts-admin-bottom')
 
