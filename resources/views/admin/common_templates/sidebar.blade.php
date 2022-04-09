39254783abef822fe797bbfc0677bb3727ec4c6d
Michael Holod <git_ara@ukr.net>
2022-04-09 03:07:21 +0300

    The sidebar template has changed in the administration panel. Added links to tags

--- resources/views/admin/common_templates/sidebar.blade.php
+++ resources/views/admin/common_templates/sidebar.blade.php
@@ -3,8 +3,13 @@
 		<h3 class="tw-text-center tw-font-black">Entity management </h3>
 		<ul class="nav flex-column">
 			<li class="nav-item">
-				<a class="nav-link active" aria-current="page" href="{{ route('articles.index') }}">
+				<a class="nav-link active" aria-current="page" href="{{ route('admin.articles.index') }}">
 					Articles
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" aria-current="page" href="{{ route('admin.tags.index') }}">
+					Tags
 				</a>
 			</li>
 		</ul>
