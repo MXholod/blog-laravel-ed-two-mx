b99e36af6f2ec5d4826da5c697a0db73fa293b5e
Michael Holod <git_ara@ukr.net>
2022-04-10 19:44:28 +0300

    The main admin page changed

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Article;
+use Illuminate\Support\Facades\DB;
 
 class AdminHomeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 	public function index(){
 		return view('admin.home.index');
 	}
+	
 }
