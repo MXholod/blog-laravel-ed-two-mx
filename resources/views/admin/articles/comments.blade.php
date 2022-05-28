8db4affb7612e2216fb9c1bfdcf1507757a7f96e
Michael Holod <git_ara@ukr.net>
2022-05-29 01:43:46 +0300

    We are using the mx-content Vue component in the comment blade template in the admin panel. This component has been registered in the admin folder and the app.js file

--- resources/views/admin/articles/comments.blade.php
+++ resources/views/admin/articles/comments.blade.php
@@ -7,28 +7,9 @@
 	<h3 class="tw-mt-2 tw-mb-2 tw-text-blue-600 tw-font-black">
 		List of comments to the article
 	</h3>
-	<ul class="list-group">
-		@foreach($comments as $comment)
-			@if ($loop->first)
-				<li class="list-group-item disabled" aria-disabled="true">
-					<div><b>User: </b>{{ $comment->user->firstname }}</div>
-					<div>
-						<b>Subject: </b>
-						{{ $comment->subject }}
-					</div>
-					<div><b>Comment: </b>{{ $comment->body }}</div>
-				</li>
-			@else
-			<li class="list-group-item">
-				<div><b>User: </b>{{ $comment->user->firstname }}</div>
-				<div>
-					<b>Subject: </b>
-					{{ $comment->subject }}
-				</div>
-				<div><b>Comment: </b>{{ $comment->body }}</div>
-			</li>
-			@endif
-		@endforeach
-	</ul>
+	<!--<ul class="list-group">-->
+	{{--  @include('admin.articles.comment_list', ['comments' => $comments]) --}}
+	<!--</ul>-->
+	<mx-content :comments="{{ json_encode($comments) }}" :paginate="5" />
 </div>
 @endsection
