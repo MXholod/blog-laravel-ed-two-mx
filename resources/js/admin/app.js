9569035b1db78ba6966c23bb64c569a1af462feb
Michael Holod <git_ara@ukr.net>
2022-05-29 01:15:21 +0300

    Vue components have been registered in file 'app.js' in 'admin' folder

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -2,10 +2,10 @@ require('./../bootstrap');
 import Vue from 'vue';
 
 //Load components by root sections: Header, Sidebar, Content, Footer
-//import Content from './components/Content.vue';
+import Content from './../components/admin/Content.vue';
 
 const app = new Vue({
     el: '#app',
-    //components: { MxContent: Content }
+    components: { MxContent: Content }
 });
 console.log("Admin");
