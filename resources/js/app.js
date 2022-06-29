651573588bafa88b6439200f4066d1e07c5120a1
Michael Holod <git_ara@ukr.net>
2022-06-29 14:33:01 +0300

    File 'app.js' changed. The vue components ArticleWiews and ArticleLikes have been registered in this file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,27 @@
 require('./bootstrap');
 import Vue from 'vue';
 
+import store from './store';
 //Load components by root sections: Header, Sidebar, Content, Footer
 import Content from './components/Content.vue';
+import ArticleViews from './components/ArticleViews.vue';
+import ArticleLikes from './components/ArticleLikes.vue';
 
 const app = new Vue({
     el: '#app',
-    components: { MxContent: Content }
+	store,
+    components: { 
+		MxContent: Content,
+		ArticleViews,
+		ArticleLikes
+	},
+	/*created(){
+		//Get slug
+		let url = window.location.pathname;
+		let index = url.lastIndexOf('/')+1;
+		let slug = url.substring(index);
+		//Initialize 
+		//this.$store.commit("SET_SLUG", slug);
+		//this.$store.dispatch("actionViews", slug);
+	}*/
 });
