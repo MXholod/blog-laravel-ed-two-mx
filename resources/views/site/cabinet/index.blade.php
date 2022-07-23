becc1c35c7315bc8b7a238b1dcb384a862b7b704
Michael Holod <git_ara@ukr.net>
2022-07-23 23:57:43 +0300

    A simple page as a cabinet for authenticated users has been created

--- resources/views/site/cabinet/index.blade.php
+++ resources/views/site/cabinet/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 <div class="tw-container tw-bg-white tw-p-6 tw-rounded-l-lg">
-	<p>Cabinet</p>
+	<div class="tw-bg-red-200 tw-p-5 tw-rounded-md">
+		<p class="tw-text-2xl tw-font-bold">User's cabinet</p>
+		@if(auth()->user()->is_admin === 1)	
+			<a href="{{ route('admin.index') }}">
+			Go to admin
+		</a>
+		@endif
+	</div>
 </div>
 @endsection
